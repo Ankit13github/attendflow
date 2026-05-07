ada1d598047b3f55c5236a68a91e4f252256bd1a
Ankit Malviya <166617168+Ankit13github@users.noreply.github.com>
2026-05-07 08:42:14 +0530

    Clean up imports in app.py
    Removed unnecessary imports and path adjustments.

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ from src.screens.student_screen import student_screen
 
 from src.components.dialog_auto_enroll import auto_enroll_dialog
 
-import sys
-import os
-sys.path.append(os.path.abspath("."))
-
-import supabase
-import pkgutil
 
 modules = [p.name for p in pkgutil.iter_modules()]
 st.write("supabase exists:", "supabase" in modules)
